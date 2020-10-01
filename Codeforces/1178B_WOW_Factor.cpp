084ec12e42723f15b6b504deae8383c9800026c9
Bhupendra Chauhan <54802156+Shinigami-Daikou@users.noreply.github.com>
2020-10-01 21:50:04 +0530

    Update 1178B_WOW_Factor.cpp

--- Codeforces/1178B_WOW_Factor.cpp
+++ Codeforces/1178B_WOW_Factor.cpp
@@ -1,38 +1,35 @@
-    #include <bits/stdc++.h>
-     
-    using namespace std;
-     
-    #define ll long long
-    #define MOD 1000000007
-     
-     
-    int main()
-    {
-        ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
-        string s;
-        cin>>s;
-        ll ans=0;
-        ll n=s.length();
-        ll l[n]={0};
-        ll total=0;
-        
-        for(ll i=2;i<n-2;i++){
-            if(s[i-2]=='v' && s[i-1]=='v')
-                l[i]=l[i-1]+1;
-            else
-                l[i]=l[i-1];
-        }
-        
-        for(ll i=0;i<n-1;i++)
-            if(s[i]=='v' && s[i+1]=='v')
-                total++;
-                
-        
-        for(ll i=2;i<n-2;i++){
-            if(s[i]=='o')
-                ans+=l[i]*(total-l[i]);
-        }
-        cout<<ans;
-        
-        return 0;
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+    string s;
+    cin>>s;
+    ll ans=0;
+    ll n=s.length();
+    ll l[n]={0};
+    ll total=0;
+
+    for(ll i=2;i<n-2;i++){
+        if(s[i-2]=='v' && s[i-1]=='v')
+            l[i]=l[i-1]+1;
+        else
+            l[i]=l[i-1];
     }
+
+    for(ll i=0;i<n-1;i++)
+        if(s[i]=='v' && s[i+1]=='v')
+            total++;
+
+
+    for(ll i=2;i<n-2;i++){
+        if(s[i]=='o')
+            ans+=l[i]*(total-l[i]);
+    }
+    cout<<ans;
+
+    return 0;
+}
